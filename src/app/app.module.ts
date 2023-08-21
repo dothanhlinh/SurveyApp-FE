c3583442a90cf718842e694a00eb5d42f1a142b2
DuAnXeNang <linhdo05022k2@gmail.com>
2023-08-21 15:10:25 +0700

    linh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ import { HttpClientModule } from '@angular/common/http';
     ClientComponent,
     AdminComponent,
     AdminObjectSurveyComponent,
-    AdminUnitTypeComponent
+    AdminUnitTypeComponent,
   ],
   imports: [
     BrowserModule,
